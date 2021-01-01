7c81e6b1ae262565b3a06e9eadd83c7c6079800d
andrea <andrecasa91@gmail.com>
2021-01-01 02:07:03 +0100

    project restyled using CMake

--- compile.sh
+++
@@ -1,5 +0,0 @@
-g++ -o ./Sample_01/Sample_01 ./Sample_01/Main_01.cpp ./src/file_handler.cpp
-
-g++ -o ./Sample_02/Sample_02 ./Sample_02/Main_02.cpp ./src/file_handler.cpp
-
-g++ -o ./Sample_03/Sample_03 ./Sample_03/Main_03.cpp ./src/file_handler.cpp
